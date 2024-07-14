844f8e97fc144cf1bd09055bb32873295ac8c875
EnzoCaetano015 <peracioenzo@gmail.com>
2024-07-14 19:14:01 -0300

    implemetação das variaveis de tamanho de tela

--- lib/sobrenos_page.dart
+++ lib/sobrenos_page.dart
@@ -20,22 +20,23 @@ class SobreNosPage extends StatefulWidget {
 class _SobreNosPageState extends State<SobreNosPage> {
   @override
   Widget build(BuildContext context) {
+    double screenWidth = MediaQuery.of(context).size.width;
+    double screenHeight = MediaQuery.of(context).size.height;
     return MaterialApp(
         debugShowCheckedModeBanner: false,
         theme: ThemeData(),
         home: Scaffold(
           appBar: AppBar1_page(
-              screenWidth: MediaQuery.of(context).size.width * 1.0,
-              destinationPage: SobrePage()),
+              screenWidth: screenWidth * 1.0, destinationPage: SobrePage()),
           endDrawer: MenuPage(),
           body: Column(children: [
             SizedBox(
-              height: MediaQuery.of(context).size.height * 0.3,
+              height: screenHeight * 0.3,
               child: Row(
                 children: [
                   Image.asset(
                     'lib/assets/banner2.png',
-                    width: MediaQuery.of(context).size.width * 1.0,
+                    width: screenWidth * 1.0,
                   )
                 ],
               ),
@@ -45,10 +46,10 @@ class _SobreNosPageState extends State<SobreNosPage> {
                 scrollDirection: Axis.vertical,
                 children: [
                   Container(
-                    height: MediaQuery.of(context).size.height * 0.05,
+                    height: screenHeight * 0.05,
                     margin: EdgeInsets.symmetric(
-                      horizontal: MediaQuery.of(context).size.width * 0.05,
-                      vertical: MediaQuery.of(context).size.height * 0.01,
+                      horizontal: screenWidth * 0.05,
+                      vertical: screenHeight * 0.01,
                     ),
                     decoration: BoxDecoration(
                       border: Border(
@@ -63,7 +64,7 @@ class _SobreNosPageState extends State<SobreNosPage> {
                         Text(
                           'Sobre Nós',
                           style: GoogleFonts.poppins(
-                            fontSize: MediaQuery.of(context).size.width * 0.064,
+                            fontSize: screenWidth * 0.064,
                             color: const Color(0xFF0E414F),
                             fontWeight: FontWeight.bold,
                           ),
@@ -76,16 +77,15 @@ class _SobreNosPageState extends State<SobreNosPage> {
                     children: [
                       Padding(
                         padding: EdgeInsets.all(
-                          MediaQuery.of(context).size.width * 0.05,
+                          screenWidth * 0.05,
                         ),
                         child: Container(
-                          width: MediaQuery.of(context).size.width *
+                          width: screenWidth *
                               0.9, // Ajuste a largura conforme necessário
                           child: Text(
                             'Somos um grupo de alunos dedicados e inovadores que uniram suas paixões pela tecnologia e educação. Movidos pelo desejo de fazer a diferença, desenvolvemos um aplicativo para a Feira Tecnológica do Estado de São Paulo (Feteps). Nossa jornada foi marcada por colaboração, aprendizado e superação de desafios. Este aplicativo é o resultado do nosso comprometimento em proporcionar uma experiência única aos participantes da Feteps, conectando pessoas, ideias e tecnologia. Estamos orgulhosos de contribuir para o sucesso deste evento e ansiosos para compartilhar essa jornada emocionante com vocês.',
                             style: GoogleFonts.poppins(
-                              fontSize:
-                                  MediaQuery.of(context).size.width * 0.04,
+                              fontSize: screenWidth * 0.04,
                               color: Colors.black,
                             ),
                             textAlign: TextAlign.center,
@@ -99,13 +99,13 @@ class _SobreNosPageState extends State<SobreNosPage> {
                     children: [
                       Image.asset(
                         'lib/assets/estudantes.png',
-                        width: MediaQuery.of(context).size.width * 0.45,
+                        width: screenWidth * 0.45,
                       )
                     ],
                   ),
                   SizedBox(
                       child: Container(
-                    height: MediaQuery.of(context).size.height * 0.01,
+                    height: screenHeight * 0.01,
                   )),
                   Row(
                     mainAxisAlignment: MainAxisAlignment.center,
